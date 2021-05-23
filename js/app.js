b9195bfd87f5f767d733d861d47fffff3d8c520d
Jatin Sangwan <jatinsangwan1120@gmail.com>
2021-05-23 20:41:40 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Home-page-02-background-animation
+gsap.fromTo(".circle1", { x: 308.5, y: 50, scale: 0.3 }, { scale: 0.5, repeat: -1, duration: 2, opacity: 0, yoyo: true });
+gsap.fromTo(".circle2", { x: 308.5, y: 500, scale: 0.3 }, { scale: 0.5, repeat: -1, duration: 2, opacity: 0, yoyo: true });
+gsap.fromTo(".cross1", { x: 458.5, y: 50, scale: 0.3 }, { scale: 0.5, repeat: -1, duration: 3, opacity: 0, rotation: 720, yoyo: true, delay: 1 });
+gsap.fromTo(".cross2", { x: 458.5, y: 500, scale: 0.3 }, { scale: 0.5, repeat: -1, duration: 3, opacity: 0, rotation: 720, yoyo: true, delay: 1 });
+gsap.fromTo(".plus1", { x: 608.5, y: 50, scale: 0.3 }, { scale: 0.5, repeat: -1, duration: 3, opacity: 0, delay: 2 });
+gsap.fromTo(".plus2", { x: 608.5, y: 500, scale: 0.3 }, { scale: 0.5, repeat: -1, duration: 3, opacity: 0, delay: 2 });
+gsap.fromTo(".ring1", { x: 758.5, y: 50, scale: 0.3 }, { scale: 0.5, repeat: -1, duration: 2, opacity: 0, yoyo: true, delay: 3 });
+gsap.fromTo(".ring2", { x: 758.5, y: 500, scale: 0.3 }, { scale: 0.5, repeat: -1, duration: 2, opacity: 0, yoyo: true, delay: 3 });
+gsap.fromTo(".star1", { x: 908.5, y: 50, scale: 0.3 }, { scale: 0.5, repeat: -1, duration: 2, opacity: 0, yoyo: true, delay: 4 });
+gsap.fromTo(".star2", { x: 908.5, y: 500, scale: 0.3 }, { scale: 0.5, repeat: -1, duration: 2, opacity: 0, yoyo: true, delay: 4 });
+// End-of-home-page-02-background-animation
